03a7466d5b5ba69abf0049c9c7261f9a1bce058c
SexWexPex <inetgameslam@gmail.com>
2018-10-06 16:08:38 +0300

    Fix issue when tabBar pushed up with keyboard

--- src/navigators/createBottomTabNavigator.js
+++ src/navigators/createBottomTabNavigator.js
@@ -1,139 +1,159 @@
 /* @flow */
 
 import * as React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Keyboard, Platform } from 'react-native';
 import { ScreenContainer } from 'react-native-screens';
 import { polyfill } from 'react-lifecycles-compat';
 import createTabNavigator, {
-  type InjectedProps,
+    type InjectedProps,
 } from '../utils/createTabNavigator';
 import BottomTabBar, { type TabBarOptions } from '../views/BottomTabBar';
 import ResourceSavingScene from '../views/ResourceSavingScene';
 
 type Props = InjectedProps & {
-  lazy?: boolean,
-  tabBarComponent?: React.ComponentType<*>,
-  tabBarOptions?: TabBarOptions,
+    lazy?: boolean,
+    tabBarComponent?: React.ComponentType<*>,
+    tabBarOptions?: TabBarOptions,
 };
 
 type State = {
-  loaded: number[],
+    loaded: number[],
 };
 
 class TabNavigationView extends React.PureComponent<Props, State> {
-  static defaultProps = {
-    lazy: true,
-  };
-
-  static getDerivedStateFromProps(nextProps, prevState) {
-    const { index } = nextProps.navigation.state;
-
-    return {
-      // Set the current tab to be loaded if it was not loaded before
-      loaded: prevState.loaded.includes(index)
-        ? prevState.loaded
-        : [...prevState.loaded, index],
+    static defaultProps = {
+        lazy: true,
     };
-  }
 
-  state = {
-    loaded: [this.props.navigation.state.index],
-  };
+    static getDerivedStateFromProps(nextProps, prevState) {
+        const { index } = nextProps.navigation.state;
 
-  _renderTabBar = () => {
-    const {
-      tabBarComponent: TabBarComponent = BottomTabBar,
-      tabBarOptions,
-      navigation,
-      screenProps,
-      getLabelText,
-      getAccessibilityLabel,
-      getButtonComponent,
-      getTestID,
-      renderIcon,
-      onTabPress,
-    } = this.props;
-
-    const { descriptors } = this.props;
-    const { state } = this.props.navigation;
-    const route = state.routes[state.index];
-    const descriptor = descriptors[route.key];
-    const options = descriptor.options;
-
-    if (options.tabBarVisible === false) {
-      return null;
+        return {
+            // Set the current tab to be loaded if it was not loaded before
+            loaded: prevState.loaded.includes(index)
+                ? prevState.loaded
+                : [...prevState.loaded, index],
+        };
     }
 
-    return (
-      <TabBarComponent
-        {...tabBarOptions}
-        jumpTo={this._jumpTo}
-        navigation={navigation}
-        screenProps={screenProps}
-        onTabPress={onTabPress}
-        getLabelText={getLabelText}
-        getButtonComponent={getButtonComponent}
-        getAccessibilityLabel={getAccessibilityLabel}
-        getTestID={getTestID}
-        renderIcon={renderIcon}
-      />
-    );
-  };
+    state = {
+        loaded: [this.props.navigation.state.index],
+        isVisible: true
+    };
 
-  _jumpTo = (key: string) => {
-    const { navigation, onIndexChange } = this.props;
+    componentDidMount(){
+        if (Platform.OS === 'android') {
+            this.keyboardEventListeners = [
+                Keyboard.addListener('keyboardDidShow', this.isVisible(false)),
+                Keyboard.addListener('keyboardDidHide', this.isVisible(true))
+            ];
+        }
+    }
 
-    const index = navigation.state.routes.findIndex(route => route.key === key);
+    componentWillUnmount() {
+        this.keyboardEventListeners && this.keyboardEventListeners.forEach((eventListener) => eventListener.remove());
+    }
 
-    onIndexChange(index);
-  };
+    isVisible = isVisible => () => this.setState({isVisible});
 
-  render() {
-    const { navigation, renderScene, lazy } = this.props;
-    const { routes } = navigation.state;
-    const { loaded } = this.state;
+    _renderTabBar = () => {
+        const {
+            tabBarComponent: TabBarComponent = BottomTabBar,
+            tabBarOptions,
+            navigation,
+            screenProps,
+            getLabelText,
+            getAccessibilityLabel,
+            getButtonComponent,
+            getTestID,
+            renderIcon,
+            onTabPress,
+        } = this.props;
 
-    return (
-      <View style={styles.container}>
-        <ScreenContainer style={styles.pages}>
-          {routes.map((route, index) => {
-            if (lazy && !loaded.includes(index)) {
-              // Don't render a screen if we've never navigated to it
-              return null;
-            }
+        const { descriptors } = this.props;
+        const { state } = this.props.navigation;
+        const route = state.routes[state.index];
+        const descriptor = descriptors[route.key];
+        const options = descriptor.options;
 
-            const isFocused = navigation.state.index === index;
-
+        if (options.tabBarVisible === false) {
+            return null;
+        }
+        if (!this.state.isVisible){
+            return null
+        }
+        else {
             return (
-              <ResourceSavingScene
-                key={route.key}
-                style={[
-                  StyleSheet.absoluteFill,
-                  { opacity: isFocused ? 1 : 0 },
-                ]}
-                isVisible={isFocused}
-              >
-                {renderScene({ route })}
-              </ResourceSavingScene>
+                <TabBarComponent
+                    {...tabBarOptions}
+                    jumpTo={this._jumpTo}
+                    navigation={navigation}
+                    screenProps={screenProps}
+                    onTabPress={onTabPress}
+                    getLabelText={getLabelText}
+                    getButtonComponent={getButtonComponent}
+                    getAccessibilityLabel={getAccessibilityLabel}
+                    getTestID={getTestID}
+                    renderIcon={renderIcon}
+                />
             );
-          })}
-        </ScreenContainer>
-        {this._renderTabBar()}
-      </View>
-    );
-  }
+        }
+    };
+
+    _jumpTo = (key: string) => {
+        const { navigation, onIndexChange } = this.props;
+
+        const index = navigation.state.routes.findIndex(route => route.key === key);
+
+        onIndexChange(index);
+    };
+
+    render() {
+        const { navigation, renderScene, lazy } = this.props;
+        const { routes } = navigation.state;
+        const { loaded } = this.state;
+
+        return (
+            <View style={styles.container}>
+                <ScreenContainer style={styles.pages}>
+                    {routes.map((route, index) => {
+                        if (lazy && !loaded.includes(index)) {
+                            // Don't render a screen if we've never navigated to it
+                            return null;
+                        }
+
+                        const isFocused = navigation.state.index === index;
+
+                        return (
+                            <ResourceSavingScene
+                                key={route.key}
+                                style={[
+                                    StyleSheet.absoluteFill,
+                                    { opacity: isFocused ? 1 : 0 },
+                                ]}
+                                isVisible={isFocused}
+                            >
+                                {renderScene({ route })}
+                            </ResourceSavingScene>
+                        );
+                    })}
+                </ScreenContainer>
+                {this._renderTabBar()}
+            </View>
+        );
+    }
 }
 
 polyfill(TabNavigationView);
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    overflow: 'hidden',
-  },
-  pages: {
-    flex: 1,
-  },
+    container: {
+        flex: 1,
+        overflow: 'hidden',
+    },
+    pages: {
+        flex: 1,
+    },
 });
 
 export default createTabNavigator(TabNavigationView);
